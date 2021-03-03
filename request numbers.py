96ef5aef4b8131ee643b82f25544695debff2db6
Hodeifha Salih <hodaeyfha@gmail.com>
2021-03-02 20:27:13 -0800

    Request number and print between then

---
+++ request numbers.py
@@ -0,0 +1,15 @@
+start = int(input('Enter start number: '))
+end = int(input('Enter end number: '))
+
+for i in range(start, 50 ,3):
+    
+    if start <= 20:
+        print(i)
+    
+print('---------')
+
+for j in range(end, 50 ,2):
+    
+    if not end <= 20:
+        print(j)
+
